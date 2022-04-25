2da9b97c611ae794640ce07bf2710fe75cbcd88b
Michael Yakovlev <kvadrofilii@gmail.com>
2022-04-25 09:39:17 +0300

    feat: add slider functional

--- shelter/js/slider.js
+++ shelter/js/slider.js
@@ -1,106 +1,188 @@
 'use strict';
 
 import { pets } from './data.js';
+import { Modal, Card } from './classes.js';
 
-const overflow = document.querySelector('.overflow'),
-  modal = document.querySelector('.modal'),
-  modalCloseBtn = modal.querySelector('.modal__button'),
-  sliderCard = document.querySelectorAll('.slider__card');
+// Считаю количество карточек в данных
+const CARD_VALUE = pets.length,
+  // Массив для генерации псевдослучайной последовательности карточек слайдера
+  ARR_ALL_CARD = addNumberForArray(CARD_VALUE),
+  // Получаю узел слайдера на главной странице
+  SLIDER_MAIN = document.querySelector('#slider-main'),
+  // Получаю узлы со слайдера на главной странице
+  CAROUSEL = SLIDER_MAIN.querySelector('.slider__carousel'),
+  MAIN_BTN_LEFT = SLIDER_MAIN.querySelector('.slider__prev'),
+  MAIN_BTN_RIGHT = SLIDER_MAIN.querySelector('.slider__next'),
+  ITEM_ACTIVE = SLIDER_MAIN.querySelector('#item-active'),
+  ITEM_LEFT = SLIDER_MAIN.querySelector('#item-left'),
+  ITEM_RIGHT = SLIDER_MAIN.querySelector('#item-right'),
+  // Получаю узел затемнения модального окна
+  MODAL_OVERFLOW = document.querySelector('#modal-overflow'),
+  // Получаю узел модального окна
+  MODAL = document.querySelector('.modal'),
+  // Получаю узел кнопки у модального окна
+  MODAL_CLOSE_BTN = MODAL.querySelector('.modal__button');
 
-// Слушаю нажатие на карточки слайдера и открываю модальное окно с динамическим наполнением
-sliderCard.forEach(card => {
-  card.addEventListener('click', (e) => {
-    const target = e.currentTarget.childNodes[3].textContent,
-      data = pets.filter(item => item.name === target)[0];
-
-    openModal(
-      data.img,
-      data.name,
-      `${data.type} ${data.breed}`,
-      data.description,
-      data.age,
-      data.inoculations,
-      data.diseases,
-      data.parasites,
-      '.modal__container'
-    );
-
-  });
-});
-
-// Закрытие модального окна при нажатии "Escape"
-document.addEventListener('keydown', e => {
-  (e.code === 'Escape') && modal.classList.contains('show') && closeModal();
-}
-);
-
-// Закрытие модального окна при нажатии на кнопку "Закрыть"
-modalCloseBtn.addEventListener('click', closeModal);
-
-// Закрытие модального окна при нажатии вне него
-overflow.addEventListener('click', e => e.target === overflow && closeModal());
+// Переменная для количества отображаемых карточек на экране
+let mediaNum = 3,
+  // Генерирую псевдослучайные числа и сохраняю первые 3
+  arrActiveCard = shuffle(ARR_ALL_CARD).slice(0, mediaNum),
+  // Создаю новый массив без чисел с главного экрана
+  arrNextCard = shuffle(filterArray(ARR_ALL_CARD, arrActiveCard)).slice(0, mediaNum);
 
 
-class Modal {
-  constructor(src, title, subtitle, text, age, inoculations, diseases, parasites, replaceSelector) {
-    this.src = src;
-    this.title = title;
-    this.subtitle = subtitle;
-    this.text = text;
-    this.age = age;
-    this.inoculations = inoculations;
-    this.diseases = diseases;
-    this.parasites = parasites;
-    this.replace = document.querySelector(replaceSelector);
-  }
+console.log('Текущий массив: ', arrActiveCard);
+console.log('Новый массив: ', arrNextCard);
 
-  render() {
-    const element = document.createElement('div');
-    element.classList.add('modal__container');
-    element.innerHTML = `
-      <img class="modal__image" src=${this.src} alt=${this.title}>
-      <div class="modal__content">
-        <h2 class="modal__title">${this.title}</h2>
-        <p class="modal__subtitle">${this.subtitle}</p>
-        <p class="modal__text">${this.text}</p>
-        <ul class="modal__list">
-          <li class="modal__item">
-            <strong>Age:</strong> ${this.age}
-          </li>
-          <li class="modal__item">
-            <strong>Inoculations:</strong> ${this.inoculations}
-          </li>
-          <li class="modal__item">
-            <strong>Diseases:</strong> ${this.diseases}
-          </li>
-          <li class="modal__item">
-            <strong>Parasites:</strong> ${this.parasites}
-          </li>
-        </ul>
-      </div>
-    `;
-    this.replace.replaceWith(element);
+
+
+// Вывожу карточки в слайдере
+const outputSliderCard = (value, arrayIndex, parentSelector) => {
+
+  for (let i = 0; i < value; i++) {
+    new Card(
+      pets[arrayIndex[i]].img,
+      pets[arrayIndex[i]].name,
+      parentSelector
+    ).render();
   }
 }
 
-// Закрытие модального окна
-function closeModal() {
-  toggle();
-  document.body.style.overflow = '';
-}
+outputSliderCard(mediaNum, arrActiveCard, '#item-active')
+
+
 
 // Открытие модально окна
-function openModal(src, title, subtitle, text, age, inoculations, diseases, parasites, replaceSelector) {
+const openModal = (src, title, subtitle, text, age, inoculations, diseases, parasites, replaceSelector) => {
 
   new Modal(
     src, title, subtitle, text, age, inoculations, diseases, parasites, replaceSelector
   ).render();
+
   toggle();
+  // Добавляю правый padding равный ширине прокрутки
+  document.body.style.paddingRight = `${window.innerWidth - document.documentElement.clientWidth}px`;
+  // Отключаю прокрутку под модальным окном
   document.body.style.overflow = 'hidden';
 }
 
+// Слушаю нажатие на карточки слайдера и открываю модальное окно с динамическим наполнением
+SLIDER_MAIN.addEventListener('click', e => {
+  // Получаю нажатую карточку
+  const card = e.target.closest('.slider__card');
+  // Если нажали мимо карточки, то прерываю функцию
+  if (!card) return;
+  // Получаю имя (title) с карточки
+  const name = card.children[1].innerHTML,
+    // Нахожу по имени нужный объект с данными на питомца
+    data = pets.filter(item => item.name === name)[0];
+  // Открываю модальное окно
+  openModal(
+    data.img,
+    data.name,
+    `${data.type} ${data.breed}`,
+    data.description,
+    data.age,
+    data.inoculations,
+    data.diseases,
+    data.parasites,
+    '.modal__container'
+  );
+});
+
+
+
+// Закрытие модального окна
+const closeModal = () => {
+  toggle();
+  // Удаляю правый padding равный ширине прокрутки
+  document.body.style.paddingRight = '';
+  // Включаю обратно прокрутку на сайте
+  document.body.style.overflow = '';
+}
+
+// Закрытие модального окна при нажатии на кнопку 'Закрыть'
+MODAL_CLOSE_BTN.addEventListener('click', closeModal);
+// Закрытие модального окна при нажатии вне него
+MODAL_OVERFLOW.addEventListener('click', e => e.target === MODAL_OVERFLOW && closeModal());
+// Закрытие модального окна при нажатии 'Escape'
+document.addEventListener('keydown', e => {
+  (e.code === 'Escape') && MODAL.classList.contains('show') && closeModal();
+}
+);
+
+
+
 // Переключение видимости для модального окна
 function toggle() {
-  overflow.classList.toggle('show');
-  modal.classList.toggle('show');
+  MODAL_OVERFLOW.classList.toggle('show');
+  MODAL.classList.toggle('show');
 }
+
+// Заполняю массив числами
+function addNumberForArray(value) {
+  const result = [];
+  for (let i = 0; i < value; i++) {
+    result.push(i)
+  }
+  return result;
+}
+
+
+// Перемешиваю массив чисел
+function shuffle(array) {
+  const result = array
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return result;
+}
+
+// Создаю новый массив без чисел в активном окне
+function filterArray(arrayAllCard, arrayActiveCard) {
+  return arrayAllCard.filter(i => !arrayActiveCard.includes(i))
+    .concat(arrayActiveCard.filter(i => !arrayAllCard.includes(i)));
+}
+
+
+
+const moveLeft = () => {
+  CAROUSEL.classList.add('transition-left');
+  MAIN_BTN_LEFT.removeEventListener('click', moveLeft);
+  MAIN_BTN_RIGHT.removeEventListener('click', moveRight);
+  outputSliderCard(mediaNum, arrNextCard, '#item-left')
+};
+
+const moveRight = () => {
+  CAROUSEL.classList.add('transition-right');
+  MAIN_BTN_LEFT.removeEventListener('click', moveLeft);
+  MAIN_BTN_RIGHT.removeEventListener('click', moveRight);
+  outputSliderCard(mediaNum, arrNextCard, '#item-right')
+};
+
+MAIN_BTN_LEFT.addEventListener('click', moveLeft);
+MAIN_BTN_RIGHT.addEventListener('click', moveRight);
+
+CAROUSEL.addEventListener('animationend', (animationEvent) => {
+  let changedItem;
+  if (animationEvent.animationName === 'move-left') {
+    CAROUSEL.classList.remove('transition-left');
+    changedItem = ITEM_LEFT;
+    ITEM_ACTIVE.innerHTML = ITEM_LEFT.innerHTML;
+  } else {
+    CAROUSEL.classList.remove('transition-right');
+    changedItem = ITEM_RIGHT;
+    ITEM_ACTIVE.innerHTML = ITEM_RIGHT.innerHTML;
+  }
+  arrActiveCard = [...arrNextCard];
+  arrNextCard = shuffle(filterArray(ARR_ALL_CARD, arrActiveCard)).slice(0, mediaNum);
+
+  console.log('Текущий массив: ', arrActiveCard);
+  console.log('Новый массив: ', arrNextCard);
+
+  changedItem.innerHTML = '';
+
+  MAIN_BTN_LEFT.addEventListener('click', moveLeft);
+  MAIN_BTN_RIGHT.addEventListener('click', moveRight);
+})
